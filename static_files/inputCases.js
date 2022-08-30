5420d1d482ee6215046f8d2826c9bdf74091d575
Slowqueso <lyndabarn2309@gmail.com>
2022-08-31 01:03:13 +0530

    Unit Testing

--- static_files/inputCases.js
+++ static_files/inputCases.js
@@ -2,9 +2,24 @@ const { getNumericDate } = require("../utils/dateConverter");
 
 const _joinActivity = {
   fail: [3, "Lynda", parseInt(getNumericDate()), 5],
-  pass: [1, "Lynda", parseInt(getNumericDate()), 3],
+  fail1: [1, "Slowqueso", parseInt(getNumericDate()), 5],
+  fail2: [1, "Lynda", parseInt(getNumericDate()), 5],
+};
+
+const _addTermForActivity = {
+  case_1: [
+    3,
+    "Privacy",
+    "Lorem ipsum dolor sit amet consectetur adipisicing elit. Natus qui quasi blanditiis libero odio fugit facilis aspernatur nemo! Rerum corrupti molestiae omnis illo debitis. Reprehenderit reiciendis eius eaque blanditiis autem.",
+  ],
+  case_2: [
+    1,
+    "Privacy",
+    "Lorem ipsum dolor sit amet consectetur adipisicing elit. Natus qui quasi blanditiis libero odio fugit facilis aspernatur nemo! Rerum corrupti molestiae omnis illo debitis. Reprehenderit reiciendis eius eaque blanditiis autem.",
+  ],
 };
 
 module.exports = {
   _joinActivity,
+  _addTermForActivity,
 };
--- test/ActivityContract.test.js
+++ test/ActivityContract.test.js
@@ -1,10 +1,14 @@
 const { assert, expect } = require("chai");
 const { network, deployments, ethers } = require("hardhat");
 const { developmentChains } = require("../hardhat-helper-config");
-const { _joinActivity } = require("../static_files/inputCases");
+const {
+  _joinActivity,
+  _addTermForActivity,
+} = require("../static_files/inputCases");
 const { convertUsdToETH } = require("../utils/usdConverter");
 const BigNumber = require("bignumber.js");
 const ethPrice = require("eth-price");
+const { getNumericDate } = require("../utils/dateConverter");
 
 describe("ActivityContract", async () => {
   let ActivityContract;
@@ -25,7 +29,7 @@ describe("ActivityContract", async () => {
     });
   });
 
-  describe("`createActivity` Test Cases \n", async () => {
+  describe("`createActivity` Test Cases", async () => {
     it("Revert Transaction if price is higher than level limit", async () => {
       try {
         const response = await ActivityContract.createActivity(
@@ -46,7 +50,7 @@ describe("ActivityContract", async () => {
       }
     });
 
-    it("Should Create an Activity Successully", async () => {
+    it("Should Create an Activity Successully \n", async () => {
       try {
         const response = await ActivityContract.createActivity(
           "Slowqueso", // _username
@@ -66,14 +70,26 @@ describe("ActivityContract", async () => {
     });
   });
 
-  describe("Testing Wei Amount", () => {
-    it("should return the right wei value", () => {
-      const weiAmount = ethers.utils.parseEther("1.0");
-      console.log(weiAmount);
-    });
-  });
-
   describe("`joinActivity` Test Cases", async () => {
+    let owner, addr2, addr3;
+    beforeEach(async () => {
+      await ActivityContract.createActivity(
+        "Slowqueso", // _username
+        "Minerva", // _title
+        "Minerva is a cryptocurrency application", // _desc
+        5, // _totalTimeInMonths
+        3, // _price
+        1, // _level
+        5, // _maxMembers
+        290822, // dateOfCreation
+        1 //_waitingPeriodInMonths
+      );
+      const [_owner, _addr2, _addr3] = await ethers.getSigners();
+      owner = _owner;
+      addr2 = _addr2;
+      addr3 = _addr3;
+    });
+
     it("`doesActivityExist` reverts with 'Activity Does not Exist`", async () => {
       ethPriceValue = await ethPrice("usd");
       const ethValue = convertUsdToETH(3, ethPriceValue);
@@ -90,21 +106,106 @@ describe("ActivityContract", async () => {
         )
       ).to.be.revertedWith("Activity Does not exist");
     });
-    it("`doesActivityExist` creates activity`", async () => {
+
+    it("`joinActivity` reverts with 'You are already a member of this activity'", async () => {
       ethPriceValue = await ethPrice("usd");
       const ethValue = convertUsdToETH(3, ethPriceValue);
       const weiAmount = ethers.utils.parseEther(ethValue.toString());
       await expect(
         ActivityContract.joinActivity(
-          _joinActivity.pass[0],
-          _joinActivity.pass[1],
-          _joinActivity.pass[2],
-          _joinActivity.pass[3],
+          _joinActivity.fail1[0],
+          _joinActivity.fail1[1],
+          _joinActivity.fail1[2],
+          _joinActivity.fail1[3],
           {
             value: weiAmount,
           }
         )
+      ).to.be.revertedWith("You are already a member of this activity");
+    });
+
+    it("`joinActivity` reverts with 'Send required ETH'", async () => {
+      ethPriceValue = await ethPrice("usd");
+      const ethValue = convertUsdToETH(4, ethPriceValue); // Sends higher value than 3 ETH
+      const weiAmount = ethers.utils.parseEther(ethValue.toString());
+      await expect(
+        ActivityContract.connect(addr2).joinActivity(
+          _joinActivity.fail2[0],
+          _joinActivity.fail2[1],
+          _joinActivity.fail2[2],
+          _joinActivity.fail2[3],
+          {
+            value: weiAmount,
+          }
+        )
+      ).to.be.revertedWith("Send required ETH");
+    });
+
+    it("`joinActivity` member is added in the activity", async () => {
+      ethPriceValue = await ethPrice("usd");
+      const ethValue = convertUsdToETH(3, ethPriceValue);
+      const weiAmount = ethers.utils.parseEther(ethValue.toString());
+      ActivityContract.connect(addr2).joinActivity(
+        _joinActivity.fail2[0],
+        _joinActivity.fail2[1],
+        _joinActivity.fail2[2],
+        _joinActivity.fail2[3],
+        {
+          value: weiAmount,
+        }
+      );
+      const response = await ActivityContract.getMemberDetails(addr2.address);
+    });
+  });
+
+  describe("\n`addTermForActivity` Test Cases", async () => {
+    let owner, addr2, addr3;
+    beforeEach(async () => {
+      await ActivityContract.createActivity(
+        "Slowqueso", // _username
+        "Minerva", // _title
+        "Minerva is a cryptocurrency application", // _desc
+        5, // _totalTimeInMonths
+        3, // _price
+        1, // _level
+        5, // _maxMembers
+        290822, // dateOfCreation
+        1 //_waitingPeriodInMonths
+      );
+      const [_owner, _addr2, _addr3] = await ethers.getSigners();
+      owner = _owner;
+      addr2 = _addr2;
+      addr3 = _addr3;
+    });
+
+    it("`addTermForActivity` reverts with 'Activity Does not exist'", async () => {
+      await expect(
+        ActivityContract.addTermForActivity(
+          _addTermForActivity.case_1[0],
+          _addTermForActivity.case_1[1],
+          _addTermForActivity.case_1[2]
+        )
       ).to.be.revertedWith("Activity Does not exist");
+    });
+
+    it("`addTermForActivity` reverts with 'You are not allowed to perform this task!", async () => {
+      await expect(
+        ActivityContract.connect(addr2).addTermForActivity(
+          _addTermForActivity.case_2[0],
+          _addTermForActivity.case_2[1],
+          _addTermForActivity.case_2[2]
+        )
+      ).to.be.revertedWith("You are not allowed to perform this task!");
+    });
+
+    it("`addTermForActivity` successfully adds a Term in the activity", async () => {
+      await ActivityContract.addTermForActivity(
+        _addTermForActivity.case_2[0],
+        _addTermForActivity.case_2[1],
+        _addTermForActivity.case_2[2]
+      );
+      const response = await ActivityContract.getTermsForActivity(1);
+      assert.isTrue(response[0].includes(_addTermForActivity.case_2[1]));
     });
   });
 });
